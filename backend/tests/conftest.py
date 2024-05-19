3f0058ad5fc414f9b85dafddd87a2731c9d11d9d
Desunovu <desunovu@gmail.com>
2024-05-19 14:17:37 +0300

    fix(backend): rework handler tests for new password logic

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,8 +1,16 @@
 import pytest
 
+from src.adapters.user_repository import UserRepository
+from src.adapters.wishlist_repository import WishlistRepository
 from src.domain.user.user import User
 from src.domain.wishlist.wishlist import Wishlist
 from src.domain.wishlist.wishlist_item import MeasurementUnit, WishlistItem, Priority
+from src.service_layer.messagebus import Messagebus
+from src.service_layer.unit_of_work import AbstractUnitOfWork
+from src.service_layer.user_handlers import COMMAND_HANDLERS, EVENT_HANDLERS
+
+
+# Domain layer
 
 
 @pytest.fixture
@@ -65,3 +73,72 @@ def populated_wishlist(user, banana_item, apple_item):
         items=[banana_item, apple_item],
     )
     return populated_wishlist
+
+
+# Service layer
+
+
+class FakeUserRepository(UserRepository):
+    def __init__(self, users: set[User]):
+        super().__init__()
+        self._users = set(users)
+
+    def _get(self, username: str) -> User | None:
+        return next((user for user in self._users if user.username == username), None)
+
+    def _add(self, user: User):
+        self._users.add(user)
+
+
+class FakeWishlistRepository(WishlistRepository):
+    def __init__(self, wishlists: set[Wishlist]):
+        super().__init__()
+        self._wishlists = set(wishlists)
+
+    def _get(self, uuid: str) -> Wishlist | None:
+        return next((wl for wl in self._wishlists if wl.uuid == uuid), None)
+
+    def _add(self, wishlist: Wishlist):
+        self._wishlists.add(wishlist)
+
+
+class FakeUnitOfWork(AbstractUnitOfWork):
+    def __init__(self):
+        super().__init__(FakeUserRepository(set()), FakeWishlistRepository(set()))
+        self.committed = False
+
+    def _commit(self):
+        self.committed = True
+
+    def _rollback(self):
+        pass
+
+
+# TODO bootstrap messagebus
+@pytest.fixture
+def messagebus():
+    return Messagebus(
+        uow=FakeUnitOfWork(),
+        command_handlers=COMMAND_HANDLERS,
+        event_handlers=EVENT_HANDLERS,
+    )
+
+
+@pytest.fixture
+def valid_password():
+    return "passWORD123"
+
+
+@pytest.fixture
+def valid_old_password():
+    return "OLDpassWORD123"
+
+
+@pytest.fixture
+def invalid_password():
+    return "123"
+
+
+@pytest.fixture
+def invalid_old_password():
+    return "123"
--- backend/tests/unit/test_handlers.py
+++ backend/tests/unit/test_handlers.py
@@ -1,92 +1,86 @@
 import pytest
 
-from src.adapters.user_repository import UserRepository
-from src.adapters.wishlist_repository import WishlistRepository
 from src.domain.user.commands import CreateUser, ChangePassword
-from src.domain.user.user import User
-from src.domain.wishlist.wishlist import Wishlist
-from src.service_layer.messagebus import Messagebus
-from src.service_layer.unit_of_work import AbstractUnitOfWork
 from src.service_layer.user_handlers import (
-    COMMAND_HANDLERS,
-    EVENT_HANDLERS,
     UserNotFound,
     UserExists,
+    InvalidOldPassword,
+    InvalidPassword,
 )
 
 
-class FakeUserRepository(UserRepository):
-    def __init__(self, users: set[User]):
-        super().__init__()
-        self._users = set(users)
-
-    def _get(self, username: str) -> User | None:
-        return next((user for user in self._users if user.username == username), None)
-
-    def _add(self, user: User):
-        self._users.add(user)
-
-
-class FakeWishlistRepository(WishlistRepository):
-    def __init__(self, wishlists: set[Wishlist]):
-        super().__init__()
-        self._wishlists = set(wishlists)
-
-    def _get(self, uuid: str) -> Wishlist | None:
-        return next((wl for wl in self._wishlists if wl.uuid == uuid), None)
-
-    def _add(self, wishlist: Wishlist):
-        self._wishlists.add(wishlist)
-
-
-class FakeUnitOfWork(AbstractUnitOfWork):
-    def __init__(self):
-        super().__init__(FakeUserRepository(set()), FakeWishlistRepository(set()))
-        self.committed = False
-
-    def _commit(self):
-        self.committed = True
-
-    def _rollback(self):
-        pass
-
-
-# TODO bootstrap messagebus
-@pytest.fixture
-def messagebus():
-    return Messagebus(
-        uow=FakeUnitOfWork(),
-        command_handlers=COMMAND_HANDLERS,
-        event_handlers=EVENT_HANDLERS,
-    )
-
-
 class TestCreateUser:
-    def test_create_new_user(self, messagebus):
-        messagebus.handle(CreateUser("testuser", "testemail@example.com", "password"))
+    def test_create_new_user(self, messagebus, valid_password):
+        messagebus.handle(
+            CreateUser("testuser", "testemail@example.com", valid_password)
+        )
 
         assert messagebus.uow.user_repository.get("testuser") is not None
 
-    def test_create_existing_user(self, messagebus):
-        messagebus.handle(CreateUser("testuser", "testemail@example.com", "password"))
+    def test_create_existing_user(self, messagebus, valid_password):
+        messagebus.handle(
+            CreateUser("testuser", "testemail@example.com", valid_password)
+        )
 
         with pytest.raises(UserExists):
             messagebus.handle(
-                CreateUser("testuser", "testemail@example.com", "password")
+                CreateUser("testuser", "testemail@example.com", valid_password)
             )
 
 
 class TestChangePassword:
-    def test_change_password(self, messagebus):
-        messagebus.handle(CreateUser("testuser", "testemail@example.com", "password"))
+    def test_change_password_by_admin(
+        self, messagebus, valid_password, valid_old_password
+    ):
+        messagebus.handle(
+            CreateUser("testuser", "testemail@example.com", valid_old_password)
+        )
         user = messagebus.uow.user_repository.get("testuser")
         old_password_hash = user.password_hash
-        messagebus.handle(ChangePassword("testuser", "newpassword"))
+        messagebus.handle(
+            ChangePassword("testuser", valid_password, called_by_admin=True)
+        )
 
         assert user.password_hash != old_password_hash
 
-    def test_change_password_non_existing_user(self, messagebus):
-        with pytest.raises(UserNotFound):
-            messagebus.handle(ChangePassword("testuser", "newpassword"))
+    def test_change_password_by_user(
+        self, messagebus, valid_password, valid_old_password
+    ):
+        messagebus.handle(
+            CreateUser("testuser", "testemail@example.com", valid_old_password)
+        )
+        user = messagebus.uow.user_repository.get("testuser")
+        old_password_hash = user.password_hash
+        messagebus.handle(
+            ChangePassword("testuser", valid_password, valid_old_password)
+        )
 
-    # TODO add test for wrong password
+        assert user.password_hash != old_password_hash
+
+    def test_change_password_non_existing_user(self, messagebus, valid_password):
+        with pytest.raises(UserNotFound):
+            messagebus.handle(
+                ChangePassword("testuser", valid_password, valid_password)
+            )
+
+    def test_change_password_invalid_old_password(
+        self, messagebus, valid_password, valid_old_password, invalid_old_password
+    ):
+        messagebus.handle(
+            CreateUser("testuser", "testemail@example.com", valid_old_password)
+        )
+        with pytest.raises(InvalidOldPassword):
+            messagebus.handle(
+                ChangePassword("testuser", valid_password, invalid_old_password)
+            )
+
+    def test_change_password_invalid_password(
+        self, messagebus, invalid_password, valid_old_password
+    ):
+        messagebus.handle(
+            CreateUser("testuser", "testemail@example.com", valid_old_password)
+        )
+        with pytest.raises(InvalidPassword):
+            messagebus.handle(
+                ChangePassword("testuser", invalid_password, valid_old_password)
+            )
