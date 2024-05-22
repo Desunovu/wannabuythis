ecdb39ff677aa5bdcf9eb1809308e1adf4fd4e8f
Desunovu <desunovu@gmail.com>
2024-05-22 22:41:09 +0300

    refactor(backend): add TYPE_CHECKING for uow non-common imports

--- backend/src/common/service/unit_of_work.py
+++ backend/src/common/service/unit_of_work.py
@@ -1,12 +1,14 @@
 import abc
+from typing import TYPE_CHECKING
 
-from src.users.adapters.user_repository import UserRepository
-from src.wishlists.adapters.wishlist_repository import WishlistRepository
+if TYPE_CHECKING:
+    from src.users.adapters.user_repository import UserRepository
+    from src.wishlists.adapters.wishlist_repository import WishlistRepository
 
 
 class AbstractUnitOfWork(abc.ABC):
     def __init__(
-        self, user_repository: UserRepository, wishlist_repository: WishlistRepository
+        self, user_repository: "UserRepository", wishlist_repository: "WishlistRepository"
     ):
         self.user_repository = user_repository
         self.wishlist_repository = wishlist_repository
