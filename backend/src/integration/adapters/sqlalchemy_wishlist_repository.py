e393e3dab229bc1bd2d5035251de231a97902de7
Desunovu <desunovu@gmail.com>
2024-05-25 12:54:07 +0300

    feat(backend): add SQLAlchemyWishlistRepository
    - Add SQLAlchemy Wishlist repository
    - Add repository tests

---
+++ backend/src/integration/adapters/sqlalchemy_wishlist_repository.py
@@ -0,0 +1,25 @@
+from typing import Type
+from uuid import UUID
+
+from sqlalchemy.orm import Session
+
+from src.wishlists.adapters.wishlist_repository import AbstractWishlistRepository
+from src.wishlists.domain.wishlist import Wishlist
+
+
+class SQLAlchemyWishlistRepository(AbstractWishlistRepository):
+    def __init__(self, session: Session):
+        super().__init__()
+        self.session = session
+
+    def _get(self, uuid: UUID) -> Wishlist | None:
+        return self.session.query(Wishlist).filter_by(uuid=uuid).first()
+
+    def _list_all(self) -> list[Type[Wishlist]]:
+        return self.session.query(Wishlist).all()
+
+    def _list_owned_by(self, username: str) -> list[Type[Wishlist]]:
+        return self.session.query(Wishlist).filter_by(owner_username=username).all()
+
+    def _add(self, user: Wishlist):
+        self.session.add(user)
--- backend/tests/integration/test_sqlalchemy_repositories.py
+++ backend/tests/integration/test_sqlalchemy_repositories.py
@@ -1,4 +1,7 @@
+import uuid
+
 from src.integration.adapters.sqlalchemy_user_repository import SQLAlchemyUserRepository
+from src.integration.adapters.sqlalchemy_wishlist_repository import SQLAlchemyWishlistRepository
 
 
 class TestSQLAlchemyUserRepository:
@@ -16,3 +19,32 @@ class TestSQLAlchemyUserRepository:
         repository = SQLAlchemyUserRepository(sqlite_session)
         repository.add(user)
         assert repository.get(user.username) is not None
+
+
+class TestSQLAlchemyWishlistRepository:
+    def test_get_wishlist(self, sqlite_session, wishlist):
+        sqlite_session.add(wishlist)
+        sqlite_session.commit()
+        repository = SQLAlchemyWishlistRepository(sqlite_session)
+        assert repository.get(wishlist.uuid) is not None
+
+    def test_get_non_existing_wishlist(self, sqlite_session):
+        repository = SQLAlchemyWishlistRepository(sqlite_session)
+        assert repository.get(uuid.uuid4()) is None
+
+    def test_list_all_wishlists(self, sqlite_session, wishlist, populated_wishlist):
+        sqlite_session.add_all([wishlist, populated_wishlist])
+        sqlite_session.commit()
+        repository = SQLAlchemyWishlistRepository(sqlite_session)
+        assert repository.list_all() == [wishlist, populated_wishlist]
+
+    def test_list_wishlists_owned_by(self, sqlite_session, user, wishlist, populated_wishlist):
+        sqlite_session.add_all([user, wishlist, populated_wishlist])
+        sqlite_session.commit()
+        repository = SQLAlchemyWishlistRepository(sqlite_session)
+        assert repository.list_owned_by(user.username) == [wishlist, populated_wishlist]
+
+    def test_add_wishlist(self, sqlite_session, wishlist):
+        repository = SQLAlchemyWishlistRepository(sqlite_session)
+        repository.add(wishlist)
+        assert repository.get(wishlist.uuid) is not None
