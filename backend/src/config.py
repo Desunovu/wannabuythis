2dfdbacd0c0e4027f010a2c74d6b2552cd000aa0
Desunovu <desunovu@gmail.com>
2024-05-25 15:44:39 +0300

    feat(backend): add config file
    - Add get_postgres_uri function

---
+++ backend/src/config.py
@@ -0,0 +1,10 @@
+import os
+
+
+def get_postgres_uri() -> str:
+    user = os.environ.get("POSTGRES_USER", "postgres")
+    password = os.environ.get("POSTGRES_PASSWORD", "postgres")
+    host = os.environ.get("POSTGRES_HOST", "localhost")
+    port = os.environ.get("POSTGRES_PORT", "5432")
+    db = os.environ.get("POSTGRES_DB", "postgres")
+    return f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}"
