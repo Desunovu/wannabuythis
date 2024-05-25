83e1de87e5ff646df032afb6dd3aeae838a7c5da
Desunovu <desunovu@gmail.com>
2024-05-25 12:19:15 +0300

    refactor(backend): use user fixture in repository tests

--- backend/tests/integration/test_sqlalchemy_repositories.py
+++ backend/tests/integration/test_sqlalchemy_repositories.py
@@ -4,23 +4,17 @@ from src.users.domain.user import User
 
 
 class TestSQLAlchemyUserRepository:
-    def test_get_user(self, sqlite_session):
-        user = User(
-            username="testuser", email="testemail@example.com", password_hash="password"
-        )
+    def test_get_user(self, sqlite_session, user):
         sqlite_session.add(user)
         sqlite_session.commit()
         repository = SQLAlchemyUserRepository(sqlite_session)
-        assert repository.get("testuser") is not None
+        assert repository.get(user.username) is not None
 
     def test_get_non_existing_user(self, sqlite_session):
         repository = SQLAlchemyUserRepository(sqlite_session)
         assert repository.get("testuser") is None
 
-    def test_add_user(self, sqlite_session):
+    def test_add_user(self, sqlite_session, user):
         repository = SQLAlchemyUserRepository(sqlite_session)
-        user = User(
-            username="testuser", email="testemail@example.com", password_hash="password"
-        )
         repository.add(user)
-        assert repository.get("testuser") is not None
+        assert repository.get(user.username) is not None
