e927500cb418ae77b326ec0599822411bd35185b
Desunovu <desunovu@gmail.com>
2024-05-26 10:43:11 +0300

    test(backend): remove domain model tests

--- backend/tests/unit/test_user_model.py
+++
@@ -1,9 +0,0 @@
-from src.users.domain.events import PasswordChanged
-
-
-def test_user_can_change_password(user):
-    """Test that a user can change their password and that the PasswordChanged event is published"""
-    user.change_password_hash("newpassword")
-
-    assert user.password_hash == "newpassword"
-    assert user.events[-1] == PasswordChanged(user.username)
