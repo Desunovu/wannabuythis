d89ae45864b14138bd2ab011fdf45a453067ff63
Desunovu <desunovu@gmail.com>
2024-06-24 16:41:21 +0300

    feat(backend): setup fastapi app for commands
    add CreateUser command endpoint

--- backend/src/integration/entrypoints/fastapi_app.py
+++ backend/src/integration/entrypoints/fastapi_app.py
@@ -1,6 +1,23 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from src import bootstrap
+from src.integration.adapters.sqlalchemy_orm import start_mappers
+from src.integration.service.sqlalchemy_uow import SQLAlchemyUnitOfWork
+from src.users.entrypoints.fastapi_auth_router import auth_router
+
+
+@asynccontextmanager
+async def lifespan(application: FastAPI):
+    start_mappers()
+    application.state.messagebus = bootstrap.bootstrap(uow=SQLAlchemyUnitOfWork())
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(auth_router)
 
 
 @app.get("/")
