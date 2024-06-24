752cdb351de9fd3c9df847a553ddc113143326c5
Desunovu <desunovu@gmail.com>
2024-06-24 16:41:21 +0300

    feat(backend): add limiter to fastapi app

--- backend/src/integration/entrypoints/fastapi_app.py
+++ backend/src/integration/entrypoints/fastapi_app.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 
 from src import bootstrap, config
 from src.common.adapters.dependencies import FakeNotificator, EmailNotificator
+from src.common.entrypoints.fastapi_limiter import limiter
 from src.integration.adapters.sqlalchemy_orm import start_mappers
 from src.integration.service.sqlalchemy_uow import SQLAlchemyUnitOfWork
 from src.users.entrypoints.fastapi_auth_router import auth_router
@@ -12,12 +13,18 @@ from src.users.entrypoints.fastapi_auth_router import auth_router
 
 @asynccontextmanager
 async def lifespan(application: FastAPI):
+    # Set up SQLAlchemy ORM
     start_mappers()
+
+    # Set up rate limiter
+    application.state.limiter = limiter
+
     # Set up dependencies for app environment
     notificator = EmailNotificator()
     if config.get_env() == "development":
         notificator = FakeNotificator()
 
+    # Set up messagebus
     application.state.messagebus = bootstrap.bootstrap(
         uow=SQLAlchemyUnitOfWork(), notificator=notificator
     )
