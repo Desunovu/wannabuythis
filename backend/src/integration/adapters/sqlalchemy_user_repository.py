131954d9b3aeba5c57d11bbce7f584ce0f21cef6
desunovu <desunovu@gmail.com>
2024-06-24 16:41:21 +0300

    fix(backend): raise notFound exceptions in SQLAlchemy repositories _get methods

--- backend/src/integration/adapters/sqlalchemy_user_repository.py
+++ backend/src/integration/adapters/sqlalchemy_user_repository.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 
+from src.common.service.exceptions import UserNotFound
 from src.users.adapters.user_repository import UserRepository
 from src.users.domain.model import User
 
@@ -9,13 +10,16 @@ class SQLAlchemyUserRepository(UserRepository):
         super().__init__()
         self.session = session
 
-    def _get(self, username: str) -> User | None:
-        return (
+    def _get(self, username: str) -> User:
+        user = (
             self.session.query(User)
             .filter_by(username=username)
             .with_for_update()
             .first()
         )
+        if not user:
+            raise UserNotFound
+        return user
 
     def _add(self, user: User):
         self.session.add(user)
