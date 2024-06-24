ffe06cfcc863d82d3818e71880f553a319e8a238
Desunovu <desunovu@gmail.com>
2024-06-24 16:41:21 +0300

    feat(backend): add JWTManager

--- backend/src/common/adapters/dependencies.py
+++ backend/src/common/adapters/dependencies.py
@@ -1,6 +1,46 @@
 import abc
+import datetime
 import hashlib
 from uuid import uuid4, UUID
+
+import jwt
+
+from src import config
+
+
+class AuthTokenManager(abc.ABC):
+    @staticmethod
+    @abc.abstractmethod
+    def generate_token(email: str) -> str: ...
+
+    @staticmethod
+    @abc.abstractmethod
+    def get_email_from_token(token: str) -> str: ...
+
+
+class JWTManager(AuthTokenManager):
+    @staticmethod
+    def generate_token(email: str) -> str:
+        token = jwt.encode(
+            {
+                "email": email,
+                "exp": datetime.datetime.now(datetime.UTC)
+                + datetime.timedelta(hours=1),
+            },
+            config.get_secret_key(),
+            algorithm="HS256",
+        )
+        return token
+
+    @classmethod
+    def get_email_from_token(cls, token: str) -> str:
+        token_payload = cls.decode_token(token)
+        return token_payload["email"]
+
+    @staticmethod
+    def decode_token(token: str) -> dict:
+        token_payload = jwt.decode(token, config.get_secret_key(), algorithms=["HS256"])
+        return token_payload
 
 
 class PasswordHasher(abc.ABC):
