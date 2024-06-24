d7e705ce30bab49c336ba5e63622d962b0328518
desunovu <desunovu@gmail.com>
2024-06-24 16:41:22 +0300

    fix(backend): fix setup in email_confirmation_sent test

--- backend/tests/unit/users/test_user_event_handlers.py
+++ backend/tests/unit/users/test_user_event_handlers.py
@@ -3,6 +3,7 @@ from src.users.domain.events import UserCreated
 
 class TestUserCreated:
     def test_email_confirmation_sent(self, capsys, messagebus, user):
+        messagebus.uow.user_repository.add(user)
         messagebus.handle(UserCreated(username=user.username, email=user.email))
         # Test what fakenotificator print message to stdout
         captured = capsys.readouterr()
