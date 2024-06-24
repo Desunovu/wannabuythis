7524d4112d092a264bc5a555185cd339ccf1e83b
Desunovu <desunovu@gmail.com>
2024-06-24 16:41:21 +0300

    feat(backend): add fastapi login route

--- backend/src/users/entrypoints/fastapi_auth_router.py
+++ backend/src/users/entrypoints/fastapi_auth_router.py
@@ -3,9 +3,20 @@ from pydantic import BaseModel
 from starlette.requests import Request
 from starlette.status import HTTP_400_BAD_REQUEST
 
+from src.common.adapters.dependencies import DefaultPasswordHasher, JWTManager
 from src.users.domain.commands import CreateUser
+from src.users.service.user_auth_service import generate_token
 
 auth_router = APIRouter()
+
+
+class LoginUserRequest(BaseModel):
+    username: str
+    password: str
+
+
+class LoginUserResponse(BaseModel):
+    token: str
 
 
 class CreateUserResponse(BaseModel):
@@ -25,3 +36,18 @@ def register(command: CreateUser, request: Request):
     except Exception as e:
         raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=str(e))
     return {"message": "User created successfully"}
+
+
+@auth_router.post("/login", response_model=LoginUserResponse)
+def login(user_data: LoginUserRequest, request: Request):
+    try:
+        token = generate_token(
+            username=user_data.username,
+            password=user_data.password,
+            password_manager=DefaultPasswordHasher(),
+            token_manager=JWTManager(),
+            uow=request.app.state.messagebus.uow,
+        )
+    except Exception as e:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=str(e))
+    return {"token": token}
