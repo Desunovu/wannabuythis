1a44db2abe1e8b2bb7f4938de1cfdd7efb697fa2
Desunovu <desunovu@gmail.com>
2024-06-24 16:41:21 +0300

    feat!(backend): add user commands fastapi routers

---
+++ backend/src/users/entrypoints/fastapi_users_command_router.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter
+from starlette.requests import Request
+from starlette.status import HTTP_200_OK
+
+from src.common.entrypoints.fastapi_dependencies import (
+    CurrentUserDependency,
+)
+from src.users.domain.commands import ChangePassword, ChangeUserEmail
+from src.users.entrypoints.fastapi_models import (
+    ChangePasswordByUserRequest,
+    ChangeEmailByUserRequest,
+)
+
+users_command_router = APIRouter(prefix="/users", tags=["user_commands"])
+
+
+@users_command_router.post("/change-password", status_code=HTTP_200_OK)
+def change_password(
+    password_data: ChangePasswordByUserRequest,
+    current_user: CurrentUserDependency,
+    request: Request,
+):
+    command = ChangePassword(
+        username=current_user.username,
+        new_password=password_data.new_password,
+        old_password=password_data.old_password,
+    )
+    request.app.state.messagebus.handle(command)
+
+
+@users_command_router.post("/change-email", status_code=HTTP_200_OK)
+def change_email(
+    email_data: ChangeEmailByUserRequest,
+    current_user: CurrentUserDependency,
+    request: Request,
+):
+    command = ChangeUserEmail(
+        username=current_user.username,
+        new_email=email_data.new_email,
+    )
+    request.app.state.messagebus.handle(command)
