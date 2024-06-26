13e753c1abf8b2e464050f281a29e1135c899636
desunovu <desunovu@gmail.com>
2024-06-26 21:20:22 +0300

    fix(tests): fix wrong boundary context role model in fixtures

--- backend/tests/unit/roles/test_role_command_handlers.py
+++ backend/tests/unit/roles/test_role_command_handlers.py
@@ -18,21 +18,21 @@ class TestCreateRole:
         messagebus.handle(CreateRole("test_role"))
         assert messagebus.uow.role_repository.get("test_role") is not None
 
-    def test_create_role_with_existing_name(self, messagebus, default_role):
-        messagebus.uow.role_repository.add(default_role)
+    def test_create_role_with_existing_name(self, messagebus, roles_default_role):
+        messagebus.uow.role_repository.add(roles_default_role)
         with pytest.raises(RoleAlreadyExists):
-            messagebus.handle(CreateRole(default_role.name))
+            messagebus.handle(CreateRole(roles_default_role.name))
 
 
 class TestAddPermissionToRole:
-    def test_add_permission_to_role(self, messagebus, default_role, permission):
-        messagebus.uow.role_repository.add(default_role)
+    def test_add_permission_to_role(self, messagebus, roles_default_role, permission):
+        messagebus.uow.role_repository.add(roles_default_role)
         messagebus.handle(
             AddPermissionToRole(
-                role_name=default_role.name, permission_name=permission.name
+                role_name=roles_default_role.name, permission_name=permission.name
             )
         )
-        assert permission in default_role.permissions
+        assert permission in roles_default_role.permissions
 
     def test_add_permission_to_role_with_non_existing_role(
         self, messagebus, permission
@@ -45,15 +45,16 @@ class TestAddPermissionToRole:
             )
 
     def test_add_permission_to_role_with_non_existing_permission(
-        self, messagebus, default_role
+        self, messagebus, roles_default_role
     ):
-        messagebus.uow.role_repository.add(default_role)
+        messagebus.uow.role_repository.add(roles_default_role)
         messagebus.handle(
             AddPermissionToRole(
-                role_name=default_role.name, permission_name="non_existing_permission"
+                role_name=roles_default_role.name,
+                permission_name="non_existing_permission",
             )
         )
-        assert Permission("non_existing_permission") in default_role.permissions
+        assert Permission("non_existing_permission") in roles_default_role.permissions
 
 
 class TestRemovePermissionFromRole:
@@ -79,13 +80,13 @@ class TestRemovePermissionFromRole:
             )
 
     def test_remove_permission_from_role_with_non_existing_permission(
-        self, messagebus, default_role
+        self, messagebus, roles_default_role
     ):
-        messagebus.uow.role_repository.add(default_role)
+        messagebus.uow.role_repository.add(roles_default_role)
         with pytest.raises(RoleDoesNotHavePermission):
             messagebus.handle(
                 RemovePermissionFromRole(
-                    role_name=default_role.name,
+                    role_name=roles_default_role.name,
                     permission_name="non_existing_permission",
                 )
             )
