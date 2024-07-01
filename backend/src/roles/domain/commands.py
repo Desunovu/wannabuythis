a3e345e51c079ef0d90d50be0d8cd11f3895dc85
desunovu <desunovu@gmail.com>
2024-07-01 22:37:59 +0300

    refactor(backend): make domain command dataclasses immutable

--- backend/src/roles/domain/commands.py
+++ backend/src/roles/domain/commands.py
@@ -3,18 +3,18 @@ from dataclasses import dataclass
 from src.common.domain.commands import Command
 
 
-@dataclass
+@dataclass(frozen=True)
 class CreateRole(Command):
     name: str
 
 
-@dataclass
+@dataclass(frozen=True)
 class AddPermissionToRole(Command):
     role_name: str
     permission_name: str
 
 
-@dataclass
+@dataclass(frozen=True)
 class RemovePermissionFromRole(Command):
     role_name: str
     permission_name: str
