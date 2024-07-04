dce015efcfeaf7d6d317dbd538a0ccfda86eb4ff
desunovu <desunovu@gmail.com>
2024-07-04 21:31:03 +0300

    feat(backend): add metadata field to commands

--- backend/src/common/domain/commands.py
+++ backend/src/common/domain/commands.py
@@ -1,6 +1,14 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from datetime import datetime, UTC
 
 
 @dataclass(frozen=True)
+class CommandMetadata:
+    username: str | None = None
+    is_superuser: bool = False
+    timestamp: datetime = datetime.now(UTC)
+
+
+@dataclass(frozen=True, kw_only=True)
 class Command:
-    pass
+    metadata: CommandMetadata = CommandMetadata()
