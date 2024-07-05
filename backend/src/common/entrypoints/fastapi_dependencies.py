5fe9e743f770ef4289cd07d3cd3cf7ef6d1da2f7
desunovu <desunovu@gmail.com>
2024-07-05 14:33:23 +0300

    feat(backend): add FastAPI dependency for wishlist ownership checking

--- backend/src/common/entrypoints/fastapi_dependencies.py
+++ backend/src/common/entrypoints/fastapi_dependencies.py
@@ -6,7 +6,9 @@ from starlette.requests import Request
 from starlette.status import HTTP_403_FORBIDDEN
 
 from src.common.dependencies.token_manager import TokenManager
+from src.common.service.exceptions import UserNotAuthorized
 from src.users.queries import user_queries
+from src.wishlists.queries import wishlist_queries
 
 if TYPE_CHECKING:
     from src.users.domain.model import User
@@ -29,6 +31,25 @@ def get_current_user(
 
 
 CurrentUserDependency = Annotated["User", Depends(get_current_user)]
+
+
+def get_wishlist_owner(
+    current_user: CurrentUserDependency,
+    request: Request,
+) -> "User":
+    """FastAPI dependency to check if current user is wishlist owner"""
+    session = request.app.state.messagebus.uow.session_factory()
+
+    wishlist = wishlist_queries.get_wishlist_by_uuid(
+        session=session, uuid=request.path_params["uuid"]
+    )
+    if current_user.username != wishlist.owner_username:
+        raise UserNotAuthorized(username=current_user.username)
+
+    return current_user
+
+
+WishlistOwnerDependency = Annotated["User", Depends(get_wishlist_owner)]
 
 
 def get_superuser(
