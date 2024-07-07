e4b646cd5a9d51e2038823e87a22b666fab7eac6
desunovu <desunovu@gmail.com>
2024-07-07 15:42:55 +0300

    Revert "feat(backend): add metadata field to commands"
    This reverts commit 388e3b1ce2a1a99c74c53c227d351b4c7928f8f8.

--- backend/src/common/domain/commands.py
+++ backend/src/common/domain/commands.py
@@ -1,14 +1,6 @@
-from dataclasses import dataclass, field
-from datetime import datetime, UTC
+from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
-class CommandMetadata:
-    username: str | None = None
-    is_superuser: bool = False
-    timestamp: datetime = datetime.now(UTC)
-
-
-@dataclass(frozen=True, kw_only=True)
 class Command:
-    metadata: CommandMetadata = CommandMetadata()
+    pass
