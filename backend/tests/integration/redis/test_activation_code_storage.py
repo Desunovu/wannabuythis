28a8d4c5c95f9727301de2c22fb0b9db5e1b85c4
Desunovu <desunovu@gmail.com>
2024-07-15 19:44:20 +0300

    fix(backend): change test activation code type to str

--- backend/tests/integration/redis/test_activation_code_storage.py
+++ backend/tests/integration/redis/test_activation_code_storage.py
@@ -1,6 +1,6 @@
 class TestRedisActivationCodeStorage:
     USERNAME = "username"
-    CODE = b"12345678"
+    CODE = "12345678"
 
     def test_get_activation_code(self, redis_client, redis_activation_code_storage):
         redis_client.set(self.USERNAME, self.CODE)
@@ -11,4 +11,4 @@ class TestRedisActivationCodeStorage:
 
     def test_save_activation_code(self, redis_client, redis_activation_code_storage):
         redis_activation_code_storage.save_activation_code(self.USERNAME, self.CODE)
-        assert redis_client.get(self.USERNAME) == self.CODE
+        assert redis_client.get(self.USERNAME).decode() == self.CODE
