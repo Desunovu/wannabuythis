969219ad5f4261b2b6437fcdf3b1c9aeef858f03
Desunovu <desunovu@gmail.com>
2024-07-15 19:41:52 +0300

    fix(backend): decode activation code from Redis

--- backend/src/integration/adapters/redis/activation_code_storage.py
+++ backend/src/integration/adapters/redis/activation_code_storage.py
@@ -15,8 +15,11 @@ class RedisActivationCodeStorage(ActivationCodeStorage):
                 db=config.REDIS_ACTIVATION_CODES_DB,
             )
 
-    def get_activation_code(self, username: str) -> str:
-        return self.redis_client.get(username)
+    def get_activation_code(self, username: str) -> None | str:
+        code = self.redis_client.get(username)
+        if code is None:
+            return None
+        return code.decode()
 
     def save_activation_code(self, username: str, code: str) -> None:
         self.redis_client.set(username, code)
