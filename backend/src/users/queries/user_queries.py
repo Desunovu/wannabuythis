729c532c3b0bcb375adb810dd0e50caac2c3f0d3
Desunovu <desunovu@gmail.com>
2024-07-16 20:26:24 +0300

    feat(backend): add query to get all users

--- backend/src/users/queries/user_queries.py
+++ backend/src/users/queries/user_queries.py
@@ -1,7 +1,12 @@
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from src.common.service.exceptions import UserNotFound
 from src.users.domain.model import User
+
+
+def get_all_users(session: Session) -> list[User]:
+    return session.scalars(select(User)).all()
 
 
 def get_user_by_username(session: Session, username: str) -> User:
