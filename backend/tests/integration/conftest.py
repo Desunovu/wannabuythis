7bb055db24c09f0a6ccdc96e8c767e1081dbfbf5
Desunovu <desunovu@gmail.com>
2024-07-17 22:40:24 +0300

    refactor(backend): improve integration pytest fixtures

--- backend/tests/integration/conftest.py
+++ backend/tests/integration/conftest.py
@@ -1,18 +1,20 @@
+import fakeredis
 import pytest
 
+from src.integration.adapters.redis.activation_code_storage import (
+    RedisActivationCodeStorage,
+)
+
+
+@pytest.fixture(scope="session")
+def redis_client() -> fakeredis.FakeRedis:
+    """Create a FakeRedis client for testing."""
+    return fakeredis.FakeRedis()
+
 
 @pytest.fixture
-def redis_client():
-    import fakeredis
-
-    redis_client = fakeredis.FakeRedis()
-    return redis_client
-
-
-@pytest.fixture
-def redis_activation_code_storage(redis_client):
-    from src.integration.adapters.redis.activation_code_storage import (
-        RedisActivationCodeStorage,
-    )
-
+def redis_activation_code_storage(
+    redis_client: fakeredis.FakeRedis,
+) -> RedisActivationCodeStorage:
+    """Create a RedisActivationCodeStorage instance using the FakeRedis client."""
     return RedisActivationCodeStorage(redis_client)
