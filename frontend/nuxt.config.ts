0ded773cb8dab3caf9d5cf1069bf78012f21afc2
desunovu <desunovu@gmail.com>
2024-07-21 14:57:19 +0300

    build(frontend): add nuxt-open-fetch

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"],
-  compatibilityDate: "2024-07-20"
+  modules: ["@nuxt/ui", "nuxt-open-fetch"],
+  compatibilityDate: "2024-07-20",
+  openFetch: {
+    clients: {
+      backend: {
+        baseURL: "http://localhost:8000",
+        schema: "http://localhost:8000/openapi.json"
+      }
+    }
+  }
 })
