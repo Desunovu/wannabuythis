f94edf0bc99746c6c7999957fb61835a3bf7f8c7
desunovu <desunovu@gmail.com>
2024-07-24 20:55:07 +0300

    build(frontend): add sidebase-auth

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "nuxt-open-fetch"],
+  modules: ["@nuxt/ui", "nuxt-open-fetch", "@sidebase/nuxt-auth"],
   compatibilityDate: "2024-07-20",
   openFetch: {
     clients: {
