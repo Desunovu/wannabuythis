89d25b216fe5c068d09c5544abb11de397971f84
desunovu <desunovu@gmail.com>
2024-07-25 19:24:29 +0300

    feat(frontend): add auth configuration

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -7,8 +7,33 @@ export default defineNuxtConfig({
     clients: {
       backend: {
         baseURL: "http://localhost:8000",
-        schema: "http://localhost:8000/openapi.json"
+        schema: "http://localhost:8000/openapi.json",
+      },
+    },
+  },
+  auth: {
+    baseURL: "http://localhost:8000/",
+    provider: {
+      type: "local",
+      endpoints: {
+        signIn: { path: "auth/login", method: "post" },
+        signOut: false,
+        signUp: { path: "auth/register", method: "post" },
+        getSession: { path: "users/me", method: "get" },
+      },
+      pages: {
+        login: "auth/login",
+      },
+      token: {
+        signInResponseTokenPointer: "/access_token",
+      },
+      session: {
+        dataType: {
+          username: "string",
+          email: "string",
+          is_active: "boolean",
+        }
       }
-    }
-  }
-})
+    },
+  },
+});
