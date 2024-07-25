f63bc72b38e028320420220213edc1c1fdded327
desunovu <desunovu@gmail.com>
2024-07-25 19:55:12 +0300

    chore(frontend): add auth debug info to the index page

---
+++ frontend/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { data } = useAuth();
+</script>
+
+<template>
+    <div v-if="data">
+        Hello, {{ data.username }}
+    </div>
+    <div v-else>
+        You are not logged in.
+    </div>
+</template>
