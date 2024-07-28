65a63289a3810a76eae7298b262c146ac2089ba4
desunovu <desunovu@gmail.com>
2024-07-28 18:35:41 +0300

    feat(frontend): prevent authenticated user from viewing login/registet page

--- frontend/pages/register.vue
+++ frontend/pages/register.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+definePageMeta({
+  auth: {
+    unauthenticatedOnly: true,
+    navigateAuthenticatedTo: "/",
+  }
+})
+</script>
+
 <template>
   <div class="flex justify-end h-screen">
     <div class="flex justify-center items-center">
