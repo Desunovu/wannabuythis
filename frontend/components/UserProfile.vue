97d001b06ea71d1767d2ce5a1de2d82096791275
desunovu <desunovu@gmail.com>
2024-07-28 13:17:57 +0300

    feat(frontend): add UserProfile component

---
+++ frontend/components/UserProfile.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import type { components } from "#build/types/open-fetch/schemas/backend.js";
+defineProps<{ userData: components["schemas"]["UserResponse"] }>();
+</script>
+
+<template>
+  <div>{{ userData.username }}'s Profile</div>
+</template>
