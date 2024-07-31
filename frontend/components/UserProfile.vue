bc79b9317143d161e1fddff4cd69fc45518afe69
desunovu <desunovu@gmail.com>
2024-07-31 18:32:02 +0300

    feat(backend): add the UCard and display more info in the UserProfile

--- frontend/components/UserProfile.vue
+++ frontend/components/UserProfile.vue
@@ -4,5 +4,18 @@ defineProps<{ userData: components["schemas"]["UserResponse"] }>();
 </script>
 
 <template>
-  <div>{{ userData.username }}'s Profile</div>
+  <UCard>
+    <template #header>
+      <div class="flex items-center space-x-4">
+        <UAvatar size="2xl" :text="userData.username.charAt(0).toUpperCase()" />
+        <div>
+          <div class="text-2xl">{{ userData.username }}</div>
+          <div>Email: {{ userData.email }}</div>
+          <div v-if="!userData.is_active" class="text-red-500">(Email not verified)</div>
+        </div>
+      </div>
+    </template>
+
+    <div>Wishlist links will be displayed here</div>
+  </UCard>
 </template>
