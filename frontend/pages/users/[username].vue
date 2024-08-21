614a52b89ee9dc03e1b1fa67d58d977fa913360d
Kirill Tatarinov <desunovu@gmail.com>
2024-08-21 11:26:30 +0300

    feat(frontend): add WishlistPreview component

--- frontend/pages/users/[username].vue
+++ frontend/pages/users/[username].vue
@@ -6,16 +6,23 @@ definePageMeta({
 const route = useRoute();
 const username = route.params.username as string;
 
-const { data } = await useBackend("/users/{username}", {
+const { data: userData } = await useBackend("/users/{username}", {
   path: {
     username: username,
   },
 });
+
+const { data: wishlistsData } = await useBackend("/wishlists/user/{username}", {
+  path: {
+    username: username,
+  }
+})
 </script>
 
 <template>
-  <div v-if="data">
-    <UserProfile :userData="data" />
+  <div v-if="userData">
+  <UserProfile v-if="userData" :userData="userData" />
+  <WishlistsPreview v-if="wishlistsData" :wishlistsData="wishlistsData" />
   </div>
   <div v-else>User {{ username }} not found.</div>
 </template>
