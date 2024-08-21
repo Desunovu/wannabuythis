2ebcc1efad8e9371bbcb5711dc22b11c49cb3fae
Kirill Tatarinov <desunovu@gmail.com>
2024-08-21 15:29:44 +0300

    feat(frontend): add a wishlist component and a page

---
+++ frontend/components/WishlistCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { components } from "#build/types/open-fetch/schemas/backend.js";
+defineProps<{
+  wishlistData: components["schemas"]["WishlistResponse"] | null;
+}>();
+</script>
+
+<template>
+  <UCard v-if="wishlistData">
+    <template #header>
+      <div class="text-2xl">{{ wishlistData.name }}</div>
+    </template>
+      <div v-for="item in wishlistData.items" :key="item.uuid">
+        <div>{{ item.name }}</div>
+        <div>{{ item.quantity }} ({{ item.measurement_unit }})</div>
+        <div>Importance: {{ item.priority }}</div>
+        <div v-if="item.is_purchased">Purchased</div>
+        <UDivider />
+      </div>
+  </UCard>
+  <div v-else>Wishlist not found</div>
+</template>
---
+++ frontend/pages/wishlists/[uuid].vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const route = useRoute();
+const wishlistUuid = route.params.uuid as string;
+
+const { data: wishlistData } = await useBackend("/wishlists/{uuid}", {
+  path: {
+    uuid: wishlistUuid,
+  },
+});
+</script>
+
+<template>
+  <WishlistFullCard :wishlistData="wishlistData" />
+</template>
