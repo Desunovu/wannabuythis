fc246522183f14664c0159123844dbcbbe57581d
desunovu <desunovu@gmail.com>
2024-08-25 17:50:10 +0300

    feat(frontend): add action buttons in user profile
    - Added change password, change email, create wishlist actions

--- frontend/pages/profile.vue
+++ frontend/pages/profile.vue
@@ -7,8 +7,53 @@ const { data: wishlistsData } = await useBackend("/wishlists/user/{username}", {
     username: username.value,
   },
 });
+
+const newEmail = ref("");
+const oldPassword = ref("");
+const newPassword = ref("");
+
+const changeEmail = async () => {
+  await useBackend("/users/me/email", {
+    method: "PATCH",
+    body: {
+      new_email: newEmail.value,
+    },
+  });
+};
+
+const changePassword = async () => {
+  await useBackend("/users/me/password", {
+    method: "PATCH",
+    body: {
+      old_password: oldPassword.value,
+      new_password: newPassword.value,
+    },
+  });
+};
+
+const createNewWishlist = async () => {
+  await useBackend("/wishlists/create", {
+    method: "POST",
+    body: {
+      wishlist_name: "New wishlist",
+    },
+  });
+};
 </script>
 
 <template>
-  <UserProfile :userData="userData" :wishlistsData="wishlistsData" />
+  <UserProfile :userData="userData" :wishlistsData="wishlistsData">
+    <template #right>
+      <UButton @click="createNewWishlist"> Create new wishlist </UButton>
+      <div>
+        <UInput v-model="newEmail" placeholder="New email" />
+        <UButton @click="changeEmail"> Change email </UButton>
+      </div>
+      <div>
+        <UInput v-model="oldPassword" placeholder="Old password" />
+        <UInput v-model="newPassword" placeholder="New password" />
+        <UButton @click="changePassword"> Change password </UButton>
+      </div>
+    </template>
+  </UserProfile>
 </template>
