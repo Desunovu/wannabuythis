1d678b96c3156ac2efd04a875839510ac42851b7
desunovu <desunovu@gmail.com>
2024-09-02 16:33:13 +0300

    feat(frontend): add unarchive button to wishlist actions

--- frontend/components/WishlistActions.vue
+++ frontend/components/WishlistActions.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 const props = defineProps<{
   wishlistUuid: string;
+  isArchived: boolean | undefined;
 }>();
 const changeWishlistName = async () => {
   await useBackend("/wishlists/change-name/{wishlist_uuid}", {
@@ -16,30 +17,40 @@ const changeWishlistName = async () => {
   // TODO: handle success
 };
 const addItem = async () => {
-    await useBackend("/wishlists/add-item/{wishlist_uuid}", {
-      method: "POST",
-      path: {
-        wishlist_uuid: props.wishlistUuid,
-      },
-      body: {
-        name: "New item",
-        quantity: 1,
-        measurement_unit: "piece",
-        priority: 1
-      },
-    })
-    // TODO: handle error
-    // TODO: handle success
+  await useBackend("/wishlists/add-item/{wishlist_uuid}", {
+    method: "POST",
+    path: {
+      wishlist_uuid: props.wishlistUuid,
+    },
+    body: {
+      name: "New item",
+      quantity: 1,
+      measurement_unit: "piece",
+      priority: 1,
+    },
+  });
+  // TODO: handle error
+  // TODO: handle success
 };
 const archiveWishlist = async () => {
-    await useBackend("/wishlists/archive/{wishlist_uuid}", {
-      method: "POST",
-      path: {
-        wishlist_uuid: props.wishlistUuid,
-      },
-    })
-    // TODO: handle error
-    // TODO: handle success
+  await useBackend("/wishlists/archive/{wishlist_uuid}", {
+    method: "POST",
+    path: {
+      wishlist_uuid: props.wishlistUuid,
+    },
+  });
+  // TODO: handle error
+  // TODO: handle success
+};
+const unarchiveWishlist = async () => {
+  await useBackend("/wishlists/unarchive/{wishlist_uuid}", {
+    method: "POST",
+    path: {
+      wishlist_uuid: props.wishlistUuid,
+    },
+  });
+  // TODO: handle error
+  // TODO: handle success
 };
 </script>
 
@@ -48,5 +59,6 @@ const archiveWishlist = async () => {
     <UButton @click="changeWishlistName"> Change name </UButton>
     <UButton @click="addItem"> Add item </UButton>
     <UButton @click="archiveWishlist"> Archive </UButton>
+    <UButton v-if="isArchived" @click="unarchiveWishlist"> Unarchive </UButton>
   </div>
 </template>
