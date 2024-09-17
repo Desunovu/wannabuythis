7afe9b75eb1f2a944cc1c4aef738e03c1eb39ffb
Kirill Tatarinov <desunovu@gmail.com>
2024-09-17 17:52:10 +0300

    feat(frontend): add inital design to the user page

--- frontend/components/UserProfileInfo.vue
+++ frontend/components/UserProfileInfo.vue
@@ -1,23 +1,21 @@
 <script setup lang="ts">
 import type { components } from "#build/types/open-fetch/schemas/backend.js";
-defineProps<{
+const { userData } = defineProps<{
   userData: components["schemas"]["UserResponse"];
 }>();
+
+const initialLetter = computed(() => userData.username.charAt(0).toUpperCase());
 </script>
 
 <template>
-  <UCard>
-    <template #header>
-      <div class="flex items-center space-x-4">
-        <UAvatar size="2xl" :text="userData.username.charAt(0).toUpperCase()" />
-        <div>
-          <div class="text-2xl">{{ userData.username }}</div>
-          <div>Email: {{ userData.email }}</div>
-          <div v-if="!userData.is_active" class="text-red-500">
-            (Email not verified)
-          </div>
-        </div>
+  <div class="flex items-center space-x-4">
+    <UAvatar size="2xl" :text="initialLetter" />
+    <div>
+      <div class="text-2xl">{{ userData.username }}</div>
+      <div>{{ userData.email }}</div>
+      <div v-if="!userData.is_active" class="text-red-500">
+        (Email not verified)
       </div>
-    </template>
-  </UCard>
+    </div>
+  </div>
 </template>
