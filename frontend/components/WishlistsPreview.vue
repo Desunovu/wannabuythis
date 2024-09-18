c9b15d19097857bbc806a23a62d1ba8b7aab1d6d
desunovu <desunovu@gmail.com>
2024-09-18 13:44:21 +0300

    feat(frontend): add an inital design for a wishlist preview

--- frontend/components/WishlistsPreview.vue
+++ frontend/components/WishlistsPreview.vue
@@ -7,8 +7,34 @@ defineProps<{
 
 <template>
   <UCard v-for="wishlist in wishlistsData" :key="wishlist.uuid">
-    <NuxtLink :to="`/wishlists/${wishlist.uuid}`">
-      {{ wishlist.name }}
-    </NuxtLink>
+    <div class="flex flex-row items-center space-x-4">
+      <div>
+        <img
+          src="https://random.imagecdn.app/500/150"
+          class="rounded-full w-16 h-16"
+          alt=""
+        />
+      </div>
+
+      <div class="flex flex-col items-baseline space-x-2">
+        <NuxtLink
+          :to="`/wishlists/${wishlist.uuid}`"
+          class="text-lg font-semibold"
+        >
+          {{ wishlist.name }}
+        </NuxtLink>
+
+        <div class="text-sm text-gray-500">
+          {{ wishlist.items.length }} items
+        </div>
+
+        <div class="text-sm text-gray-500">
+          Created at {{ new Date(wishlist.created_at).toLocaleDateString() }}
+          {{ wishlist.is_archived ? "(archived)" : "" }}
+        </div>
+
+
+      </div>
+    </div>
   </UCard>
 </template>
