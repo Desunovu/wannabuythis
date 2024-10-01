fcf20c7ab6ff45dcb2eeffac2510f7f5a985d9b7
Kirill Tatarinov <desunovu@gmail.com>
2024-10-01 22:24:45 +0300

    feat(frontend): make item card use new composable and remove item actions

--- frontend/components/WishlistItemCard.vue
+++ frontend/components/WishlistItemCard.vue
@@ -1,47 +1,18 @@
 <script setup lang="ts">
 import type { components } from "#build/types/open-fetch/schemas/backend.js";
+
 const props = defineProps<{
   wishlistItem: components["schemas"]["WishlistItemResponse"];
-  isOwner: boolean;
 }>();
 
-const priorityColor = computed(() => {
-  const priorities = {
-    0: "text-neutral-500",
-    1: "text-yellow-500",
-    2: "text-orange-500",
-    3: "text-red-500",
-  };
-
-  return priorities[props.wishlistItem.priority];
-});
-
-const priorityText = computed(() => {
-  const priorities = {
-    0: "",
-    1: "Low priority",
-    2: "Medium priority",
-    3: "High priority",
-  };
-
-  return priorities[props.wishlistItem.priority];
-});
-
-const amountText = computed(() => {
-  if (props.wishlistItem.measurement_unit === "piece") {
-    if (props.wishlistItem.quantity === 1) {
-      return "1 piece";
-    }
-    return `${props.wishlistItem.quantity} pieces`;
-  }
-
-  return `${props.wishlistItem.quantity} ${props.wishlistItem.measurement_unit}`;
-});
+const { priorityColor, priorityText, amountText } = useWishlistItemStyles(
+  props.wishlistItem
+);
 </script>
 
 <template>
   <UCard>
-    <div class="flex flex-col items-center space-y-2 h-56">
+    <div class="flex flex-col items-center space-y-2 h-46">
       <UAvatar size="2xl" :text="wishlistItem.name.charAt(0)" />
       <div class="text-2xl font-bold">{{ wishlistItem.name }}</div>
       <div>{{ amountText }}</div>
@@ -50,13 +21,5 @@ const amountText = computed(() => {
         Purchased
       </div>
     </div>
-
-    <!-- Only show actions if the user is the owner -->
-    <WishlistItemActions
-      v-if="isOwner"
-      :wishlist-uuid="wishlistItem.wishlist_uuid"
-      :wishlist-item-uuid="wishlistItem.uuid"
-      :is-purchased="wishlistItem.is_purchased"
-    />
   </UCard>
 </template>
