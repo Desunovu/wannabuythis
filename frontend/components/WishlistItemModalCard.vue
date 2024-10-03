20fe97bfc6b447357372ed1af8ac88368bf23ed7
Kirill Tatarinov <desunovu@gmail.com>
2024-10-03 16:20:49 +0300

    feat(frontend): control WishlistItemModal programmatically

--- frontend/components/WishlistItemModalCard.vue
+++ frontend/components/WishlistItemModalCard.vue
@@ -2,7 +2,7 @@
 import type { components } from "#build/types/open-fetch/schemas/backend.js";
 
 const props = defineProps<{
-  wishlistItem: components["schemas"]["WishlistItemResponse"] | null;
+  wishlistItem: components["schemas"]["WishlistItemResponse"];
   isOwner: boolean;
 }>();
 
@@ -12,25 +12,31 @@ const { priorityColor, priorityText, amountText } = useWishlistItemStyles(
 </script>
 
 <template>
-  <UCard v-if="wishlistItem">
-    <div class="flex flex-col items-center space-y-8">
-      <div class="flex flex-col items-center space-y-2">
-        <UAvatar class="rounded-none" size="3xl" :text="wishlistItem.name.charAt(0)" />
-        <div class="text-2xl font-bold">{{ wishlistItem.name }}</div>
-        <div>{{ amountText }}</div>
-        <div :class="priorityColor">{{ priorityText }}</div>
-        <div v-if="wishlistItem.is_purchased" class="text-red-600 font-bold">
-          Purchased
+  <UModal>
+    <UCard>
+      <div class="flex flex-col items-center space-y-8">
+        <div class="flex flex-col items-center space-y-2">
+          <UAvatar
+            class="rounded-none"
+            size="3xl"
+            :text="wishlistItem.name.charAt(0)"
+          />
+          <div class="text-2xl font-bold">{{ wishlistItem.name }}</div>
+          <div>{{ amountText }}</div>
+          <div :class="priorityColor">{{ priorityText }}</div>
+          <div v-if="wishlistItem.is_purchased" class="text-red-600 font-bold">
+            Purchased
+          </div>
         </div>
-      </div>
 
-      <!-- Only show actions if the user is the owner -->
-      <WishlistItemActions
-        v-if="isOwner"
-        :wishlist-uuid="wishlistItem.wishlist_uuid"
-        :wishlist-item-uuid="wishlistItem.uuid"
-        :is-purchased="wishlistItem.is_purchased"
-      />
-    </div>
-  </UCard>
+        <!-- Only show actions if the user is the owner -->
+        <WishlistItemActions
+          v-if="isOwner"
+          :wishlist-uuid="wishlistItem.wishlist_uuid"
+          :wishlist-item-uuid="wishlistItem.uuid"
+          :is-purchased="wishlistItem.is_purchased"
+        />
+      </div>
+    </UCard>
+  </UModal>
 </template>
