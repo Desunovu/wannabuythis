aac7ca666e43169c44910884323d387f283df1c5
Kirill Tatarinov <desunovu@gmail.com>
2024-10-06 04:13:00 +0300

    feat(frontend): remove card ui component from profile

--- frontend/components/Profile.vue
+++ frontend/components/Profile.vue
@@ -7,15 +7,13 @@ defineProps<{
 </script>
 
 <template>
-  <UCard>
-    <div class="flex items-start justify-between my-4">
-      <ProfileInfo v-if="userData" :userData="userData" class="basis-3/4" />
-      <slot name="right" class="basis-1/4" />
-    </div>
-    <div class="flex flex-col space-y-4 mt-4">
-      <h1 class="text-2xl font-bold">Wishlists</h1>
-      <WishlistsPreview v-if="wishlistsData" :wishlistsData="wishlistsData" />
-      <slot name="bottom" />
-    </div>
-  </UCard>
+  <div class="flex items-start justify-between my-4">
+    <ProfileInfo v-if="userData" :userData="userData" class="basis-3/4" />
+    <slot name="right" class="basis-1/4" />
+  </div>
+  <div class="flex flex-col space-y-4 mt-4">
+    <h1 class="text-2xl font-bold">Wishlists</h1>
+    <WishlistsPreview v-if="wishlistsData" :wishlistsData="wishlistsData" />
+    <slot name="bottom" />
+  </div>
 </template>
