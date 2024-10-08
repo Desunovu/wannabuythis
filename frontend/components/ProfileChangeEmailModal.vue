df0291c16f689119046de68bf0716cbda6624c76
Kirill Tatarinov <desunovu@gmail.com>
2024-10-08 18:08:33 +0300

    feat(frontend): add modal components for profile editing

---
+++ frontend/components/ProfileChangeEmailModal.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const modal = useModal();
+
+const newEmail = ref("");
+
+const changeEmail = async () => {
+  await useBackend("/users/me/email", {
+    method: "PATCH",
+    body: {
+      new_email: newEmail.value,
+    },
+  });
+
+  newEmail.value = "";
+  modal.close();
+  // TODO: handle error
+  // TODO: handle success
+};
+</script>
+
+<template>
+  <UModal>
+    <UCard>
+      <div class="space-y-4">
+        <div>Changing email...</div>
+        <UInput block v-model="newEmail" placeholder="New email" />
+        <UButton block @click="changeEmail"> Change email </UButton>
+      </div>
+    </UCard>
+  </UModal>
+</template>
---
+++ frontend/components/ProfileEditModal.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import ProfileChangeEmailModal from './ProfileChangeEmailModal.vue';
+import ProfileChangePasswordModal from './ProfileChangePasswordModal.vue';
+
+const modal = useModal();
+
+const openChangeEmailModal = () => {
+  modal.close();
+  setTimeout(() => modal.open(ProfileChangeEmailModal), 400);
+};
+
+const openChangePasswordModal = () => {
+  modal.close();
+  setTimeout(() => modal.open(ProfileChangePasswordModal), 400);
+};
+</script>
+
+<template>В
+  <UModal>
+    <UCard>
+      <div class="space-y-4">
+        <UButton block @click="openChangeEmailModal"> Change email </UButton>
+        <UButton block @click="openChangePasswordModal">
+          Change password
+        </UButton>
+      </div>
+    </UCard>
+  </UModal>
+</template>
