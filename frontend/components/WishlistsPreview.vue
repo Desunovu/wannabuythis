cb6e3e2593f88ec611c193758facc252b420e462
desunovu <desunovu@gmail.com>
2024-10-09 20:04:34 +0300

    feat(frontend): improve wishlist preview
    - remove null type from props
    - add header
    - remove image
    - hide creation date and item amount until a preview is not hovered

--- frontend/components/WishlistsPreview.vue
+++ frontend/components/WishlistsPreview.vue
@@ -1,39 +1,31 @@
 <script setup lang="ts">
 import type { components } from "#build/types/open-fetch/schemas/backend.js";
 defineProps<{
-  wishlistsData: components["schemas"]["WishlistResponse"][] | null;
+  wishlistsData: components["schemas"]["WishlistResponse"][];
 }>();
 </script>
 
 <template>
+  <h1 v-if="wishlistsData.length" class="text-2xl font-bold">Wishlists</h1>
+  <div v-if="wishlistsData.length === 0">No wishlists found</div>
+
+  <div v-if="!wishlistsData">No wishlists found</div>
   <UCard v-for="wishlist in wishlistsData" :key="wishlist.uuid">
-    <div class="flex flex-row items-center space-x-4">
-      <div>
-        <img
-          src="https://random.imagecdn.app/500/150"
-          class="rounded-full w-16 h-16"
-          alt=""
-        />
+    <div class="flex flex-col group items-baseline space-x-2">
+      <NuxtLink
+        :to="`/wishlists/${wishlist.uuid}`"
+        class="text-lg font-semibold"
+      >
+        {{ wishlist.name }}
+      </NuxtLink>
+
+      <div class="hidden group-hover:block text-sm text-gray-500">
+        {{ wishlist.items.length }} items
       </div>
 
-      <div class="flex flex-col items-baseline space-x-2">
-        <NuxtLink
-          :to="`/wishlists/${wishlist.uuid}`"
-          class="text-lg font-semibold"
-        >
-          {{ wishlist.name }}
-        </NuxtLink>
-
-        <div class="text-sm text-gray-500">
-          {{ wishlist.items.length }} items
-        </div>
-
-        <div class="text-sm text-gray-500">
-          Created at {{ new Date(wishlist.created_at).toLocaleDateString() }}
-          {{ wishlist.is_archived ? "(archived)" : "" }}
-        </div>
-
-
+      <div class="text-sm hidden group-hover:block text-gray-500">
+        Created at {{ new Date(wishlist.created_at).toLocaleDateString() }}
+        {{ wishlist.is_archived ? "(archived)" : "" }}
       </div>
     </div>
   </UCard>
