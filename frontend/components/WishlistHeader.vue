36b86aa6c823388ee1c9010cb631c952608c22ce
desunovu <desunovu@gmail.com>
2024-10-10 15:51:19 +0300

    feat(frontend): add the WishlistHeader component to replae the old card info

---
+++ frontend/components/WishlistHeader.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import type { components } from "#build/types/open-fetch/schemas/backend.js";
+import WishlistActionButtonsModal from "./WishlistActionButtonsModal.vue";
+
+const props = defineProps<{
+  wishlistData: components["schemas"]["WishlistResponse"] | null;
+  isOwner: boolean;
+}>();
+
+const modal = useModal();
+
+const openWishlistActionsModal = (
+  wishlist: components["schemas"]["WishlistResponse"]
+) => {
+  modal.open(WishlistActionButtonsModal, {
+    wishlistUuid: props.wishlistData!.uuid,
+    isArchived: props.wishlistData!.is_archived,
+  });
+};
+</script>
+
+<template>
+  <div v-if="wishlistData" class="flex items-start justify-between py-4">
+    <!-- Wishlist Info block -->
+    <div>
+      <div class="text-4xl font-bold text-gray-100">
+        {{ wishlistData.name }}
+      </div>
+
+      <div class="text-sm text-gray-500">
+        {{ wishlistData.items.length }} items
+      </div>
+
+      <div class="text-sm text-gray-500">
+        Created at {{ new Date(wishlistData.created_at).toLocaleDateString() }}
+        {{ wishlistData.is_archived ? "(archived)" : "" }}
+      </div>
+
+      <div v-if="wishlistData.is_archived" class="text-sm text-gray-500">
+        (Wishlist Archived)
+      </div>
+    </div>
+
+    <!-- Only show actions if the user is the owner -->
+    <UButton
+      v-if="isOwner"
+      label="Edit wishlist"
+      @click="openWishlistActionsModal(wishlistData)"
+      class="self-start"
+    />
+  </div>
+</template>
