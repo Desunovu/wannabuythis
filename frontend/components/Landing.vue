adcf011cd0a8119bf5f085fd23c8cb03842a67c5
Kirill Tatarinov <desunovu@gmail.com>
2024-10-16 19:00:03 +0300

    feat(frontend): add Landing component

---
+++ frontend/components/Landing.vue
@@ -0,0 +1,23 @@
+
+
+<template>
+  <div
+    class="container mx-auto p-4 flex flex-col items-center justify-center h-screen"
+  >
+    <div class="text-6xl font-bold mb-8">
+      Record, manage, and share your wishlist — all in one place.
+    </div>
+
+    <div class="text-xl mb-12">
+      Wannabuythis is a service designed to help you create smart wishlists that
+      assist in tracking purchase priorities. Additionally, the platform allows
+      you to share your lists with others.
+    </div>
+
+    <div class="self-end space-x-4">
+      <UButton class="px-8 py-2" to="/login">
+        Start Using the Service
+      </UButton>
+    </div>
+  </div>
+</template>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -9,6 +9,5 @@ const { data } = useAuth();
 </script>
 
 <template>
-  <div v-if="data">Hello, {{ data.username }}</div>
-  <div v-else>You are not logged in.</div>
+  <Landing />
 </template>
