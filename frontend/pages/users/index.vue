d2c6bd5d002400b9f022aba200c355523a26243a
desunovu <desunovu@gmail.com>
2024-10-25 13:39:34 +0300

    feat(frontend): add UserPreview components

--- frontend/pages/users/index.vue
+++ frontend/pages/users/index.vue
@@ -3,14 +3,16 @@ definePageMeta({
   auth: false,
 });
 
-const { data } = await useBackend("/users/", {});
+const { data: userData } = await useBackend("/users/", {});
 </script>
 
 <template>
-  <div v-if="data">
+  <!-- <div v-if="data">
     <div v-for="user in data" :key="user.username">
       <NuxtLink :to="`/users/${user.username}`">{{ user.username }}</NuxtLink>
     </div>
   </div>
-  <div v-else>No users found.</div>
+  <div v-else>No users found.</div> -->
+  <UserPreviewGrid :users="userData" />
+  <div v-if="userData?.length === 0">No users found.</div>
 </template>
