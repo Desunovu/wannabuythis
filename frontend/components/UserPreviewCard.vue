5289878c6ced45a8d1bb7df66dc4051b9f89c31f
desunovu <desunovu@gmail.com>
2024-10-25 13:39:34 +0300

    feat(frontend): add UserPreview components

---
+++ frontend/components/UserPreviewCard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { components } from "#build/types/open-fetch/schemas/backend.js";
+
+const props = defineProps<{
+  user: components["schemas"]["UserResponse"];
+}>();
+</script>
+
+<template>
+  <UCard @click="navigateTo(`/users/${user.username}`)">
+    <div class="flex flex-col items-center cursor-pointer space-y-4">
+      <UAvatar size="2xl" :text="user.username.charAt(0)" class="rounded-lg" />
+      <div class="text-2xl font-bold text-center">
+        {{ user.username }}
+      </div>
+    </div>
+  </UCard>
+</template>
---
+++ frontend/components/UserPreviewGrid.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { components } from "#build/types/open-fetch/schemas/backend.js";
+
+const props = defineProps<{
+  users: components["schemas"]["UserResponse"][];
+}>();
+</script>
+
+<template>
+  <div
+    class="grid grid-cols-1 md:grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4"
+  >
+    <UserPreviewCard
+      v-for="user in users"
+      :user="user"
+      :key="user.username"
+      @click="navigateTo(`/users/${user.username}`)"
+    />
+  </div>
+</template>
